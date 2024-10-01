051a69760184c4097685c8edca751d83e1b8c605
Jonathan Schort <e12225164@student.tuwien.ac.at>
2024-10-01 15:14:09 +0200

    feat: new try with correct url

--- wp-content/cache/asset-cleanup/js/item/spar-fiby__js__custom-header-js-v24ad94e47de556fe1b8ea225714493968fbceb61.js
+++
@@ -1,5 +0,0 @@
-/*!/wp-content/themes/spar-fiby/js/custom-header.js*/
-document.addEventListener("DOMContentLoaded",function(){const sections=document.querySelectorAll('.sections');console.log(sections);const navLinks=document.querySelectorAll('.elementor-nav-menu .menu-item a');function getVisibleSection(){let maxVisibleHeight=0;let mostVisibleSectionId=null;console.log(mostVisibleSectionId);sections.forEach(section=>{const rect=section.getBoundingClientRect();const visibleHeight=Math.max(0,Math.min(rect.bottom,window.innerHeight)-Math.max(rect.top,0));if(visibleHeight>maxVisibleHeight){maxVisibleHeight=visibleHeight;mostVisibleSectionId=section.getAttribute('id')}
-console.log(mostVisibleSectionId)});navLinks.forEach(link=>{link.classList.remove('navbar-active');if(mostVisibleSectionId&&link.getAttribute('href')===`#${mostVisibleSectionId}`){link.classList.add('navbar-active')}})}
-window.addEventListener('scroll',getVisibleSection);window.addEventListener('resize',getVisibleSection);getVisibleSection()})
-;
